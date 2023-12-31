ac8be290eddd2a0026c082b339abb3912f57f515
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-12-31 19:33:00 +0700

    fix: link in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <li>when folder empty it will be delete.</li>
             <li>This is only front-end for work with firebase, it can not get file by curl or wget in terminal.</li>
             <li>This is website for share files. </li>
-            <li>go to source <a href="/source">click</a></li>
+            <li>go to source <a href="/source/">click</a></li>
         </ul>
     </div>
 }
